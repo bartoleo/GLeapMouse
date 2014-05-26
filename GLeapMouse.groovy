c5eac68464d5b96ffdf1c3a3949a5d462beb687d
Leo Bartoloni <bartololeo74@gmail.com>
2014-05-26 17:07:00 +0200

    first commit

---
+++ GLeapMouse.groovy
@@ -0,0 +1,162 @@
+import com.leapmotion.leap.*
+import com.leapmotion.leap.Gesture.State
+import java.awt.Robot
+import java.awt.event.InputEvent
+import java.awt.GraphicsDevice
+import java.awt.GraphicsEnvironment
+
+class MousePointerListener extends Listener {
+
+    Robot robot = new Robot()
+    GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    int width = gd.getDisplayMode().getWidth();
+    int height = gd.getDisplayMode().getHeight();
+
+    public void onInit(Controller controller) {
+        println "Initialized"
+    }
+
+    public void onConnect(Controller controller) {
+        println "Connected"
+        controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+        controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
+        controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+        controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
+    }
+
+    public void onDisconnect(Controller controller) {
+        //Note: not dispatched when running in a debugger.
+        println "Disconnected"
+    }
+
+    public void onExit(Controller controller) {
+        println "Exited"
+    }
+
+    public void onFrame(Controller controller) {
+        // Get the most recent frame and report some basic information
+        Frame frame = controller.frame();
+        System.out.println("Frame id: " + frame.id()
+                         + ", timestamp: " + frame.timestamp()
+                         + ", hands: " + frame.hands().count()
+                         + ", fingers: " + frame.fingers().count()
+                         + ", tools: " + frame.tools().count()
+                         + ", gestures " + frame.gestures().count());
+
+        if (!frame.hands().isEmpty()) {
+            // Get the first hand
+            Hand hand = frame.hands().get(0);
+
+            // Check if the hand has any fingers
+            FingerList fingers = hand.fingers();
+            if (!fingers.isEmpty()) {
+                // Calculate the hand's average finger tip position
+                Vector avgPos = Vector.zero();
+                for (Finger finger : fingers) {
+                    avgPos = avgPos.plus(finger.tipPosition());
+                }
+                avgPos = avgPos.divide(fingers.count());
+                System.out.println("Hand has " + fingers.count()
+                                 + " fingers, average finger tip position: " + avgPos);
+                robot.mouseMove((avgPos.x/150*width+width/2) as int, (height*1.2-avgPos.y/150*height) as int)
+            }
+
+            // Get the hand's sphere radius and palm position
+            System.out.println("Hand sphere radius: " + hand.sphereRadius()
+                             + " mm, palm position: " + hand.palmPosition());
+
+            // Get the hand's normal vector and direction
+            Vector normal = hand.palmNormal();
+            Vector direction = hand.direction();
+
+            // Calculate the hand's pitch, roll, and yaw angles
+            System.out.println("Hand pitch: " + Math.toDegrees(direction.pitch()) + " degrees, "
+                             + "roll: " + Math.toDegrees(normal.roll()) + " degrees, "
+                             + "yaw: " + Math.toDegrees(direction.yaw()) + " degrees");
+        }
+
+        GestureList gestures = frame.gestures();
+        for (int i = 0; i < gestures.count(); i++) {
+            Gesture gesture = gestures.get(i);
+
+            switch (gesture.type()) {
+                case Gesture.Type.TYPE_CIRCLE:
+                    CircleGesture circle = new CircleGesture(gesture);
+
+                    // Calculate clock direction using the angle between circle normal and pointable
+                    String clockwiseness;
+                    if (circle.pointable().direction().angleTo(circle.normal()) <= Math.PI/4) {
+                        // Clockwise if angle is less than 90 degrees
+                        clockwiseness = "clockwise";
+                    } else {
+                        clockwiseness = "counterclockwise";
+                    }
+
+                    // Calculate angle swept since last frame
+                    double sweptAngle = 0;
+                    if (circle.state() != State.STATE_START) {
+                        CircleGesture previousUpdate = new CircleGesture(controller.frame(1).gesture(circle.id()));
+                        sweptAngle = (circle.progress() - previousUpdate.progress()) * 2 * Math.PI;
+                    }
+
+                    System.out.println("Circle id: " + circle.id()
+                               + ", " + circle.state()
+                               + ", progress: " + circle.progress()
+                               + ", radius: " + circle.radius()
+                               + ", angle: " + Math.toDegrees(sweptAngle)
+                               + ", " + clockwiseness);
+                    break;
+                case Gesture.Type.TYPE_SWIPE:
+                    SwipeGesture swipe = new SwipeGesture(gesture);
+                    System.out.println("Swipe id: " + swipe.id()
+                               + ", " + swipe.state()
+                               + ", position: " + swipe.position()
+                               + ", direction: " + swipe.direction()
+                               + ", speed: " + swipe.speed());
+                    break;
+                case Gesture.Type.TYPE_SCREEN_TAP:
+                    ScreenTapGesture screenTap = new ScreenTapGesture(gesture);
+                    System.out.println("Screen Tap id: " + screenTap.id()
+                               + ", " + screenTap.state()
+                               + ", position: " + screenTap.position()
+                               + ", direction: " + screenTap.direction());
+                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                    break;
+                case Gesture.Type.TYPE_KEY_TAP:
+                    KeyTapGesture keyTap = new KeyTapGesture(gesture);
+                    System.out.println("Key Tap id: " + keyTap.id()
+                               + ", " + keyTap.state()
+                               + ", position: " + keyTap.position()
+                               + ", direction: " + keyTap.direction());
+                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                
+                    break;
+                default:
+                    System.out.println("Unknown gesture type.");
+                    break;
+            }
+        }
+
+        if (!frame.hands().isEmpty() || !gestures.isEmpty()) {
+            System.out.println();
+        }
+    }
+}
+
+// Create a sample listener and controller
+MousePointerListener listener = new MousePointerListener();
+Controller controller = new Controller();
+
+// Have the sample listener receive events from the controller
+controller.addListener(listener);
+
+// Keep this process running until Enter is pressed
+System.out.println("Press Enter to quit...");
+try {
+    System.in.read();
+} catch (IOException e) {
+    e.printStackTrace();
+}
+
+// Remove the sample listener when done
+controller.removeListener(listener);
